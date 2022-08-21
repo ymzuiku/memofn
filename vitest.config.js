7f1829b408645dddc7749a2ddbea93e4a801b925
pillar <ymblender@gmail.com>
2022-08-21 17:00:50 +0800

    fix: add test

---
+++ vitest.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("vitest/config");
+module.exports = defineConfig({
+  define: {
+    "import.meta.vitest": "undefined",
+  },
+  test: {
+    includeSource: ["**/*.{ts,tsx}"],
+  },
+});
